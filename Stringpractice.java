6e011f6b63cae5600b5140ad928d8c70c704a48d
Binuwara-D-B-Y <IT23184558@my.sliit.lk>
2024-07-23 20:35:03 +0530

    Your commit message

--- Stringpractice.java
+++ Stringpractice.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 
 public class Stringpractice {
     private static int n; // Number of students
@@ -7,26 +7,35 @@ public class Stringpractice {
 
     public static void main(String[] args) {
 	Scanner scan = new Scanner(System.in);
-		
+
 	System.out.print("Enter your first name: ");
 	String firstName = scan.next();
-	System.out.print("Enter your middle name: ");
+    System.out.print("Enter your middle name: ");
 	String middleName = scan.next();
 	System.out.print("Enter your Last name: ");
 	String lastName = scan.next();
 
-	StringBuilder sb = new StringBuilder();
+    StringBuilder sb = new StringBuilder();
+    sb.append(lastName + ", ");
 	sb.append(firstName + " ");
-	sb.append(middleName + " ");
-	sb.append(lastName);
-	String result = sb.toString();
-	System.out.print("Full name is " + result);
+    sb.append(middleName);
+    String nameResult = sb.toString();
+	
+    System.out.print("Full name is: " + nameResult);
 
-	System.out.print("Enter your full name: ");
+	System.out.println("\nEnter another full name: ");
 	String fullName = scan.next();
 	
-	String str1 = result; 
-	String str2 = fullName; 
-	String str3 = new String 
+	if (fullName.equalsIgnoreCase(nameResult)) {
+        System.out.println("The names are the same.");
+    } else {
+        System.out.println("The names are different.");
+    }
+
+    System.out.println("Part 03;\n");
+    String replacedName = nameResult.replace('a', '@').replace('e', '3');
+    String.out.printf("Name after replacement: %s", replacedName);
+
+
 	}
 }
