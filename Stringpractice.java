88e6fb17590853a16aed9222829250fadc74b839
Binuwara-D-B-Y <IT23184558@my.sliit.lk>
2024-07-25 01:08:07 +0530

    Your commit message

--- Stringpractice.java
+++ Stringpractice.java
@@ -1,14 +1,10 @@
     import java.util.Scanner;
 
 public class Stringpractice {
-    private static int n; // Number of students
-    private static int[][] marks;
-    private static int studentID, subjectID;
-
     public static void main(String[] args) {
 	Scanner scan = new Scanner(System.in);
 
-	System.out.print("Enter your first name: ");
+	System.out.print("\nEnter your first name: ");
 	String firstName = scan.next();
     System.out.print("Enter your middle name: ");
 	String middleName = scan.next();
@@ -16,26 +12,32 @@ public class Stringpractice {
 	String lastName = scan.next();
 
     StringBuilder sb = new StringBuilder();
-    sb.append(lastName + ", ");
+    sb.append(lastName + " ");
 	sb.append(firstName + " ");
     sb.append(middleName);
     String nameResult = sb.toString();
 	
-    System.out.print("Full name is: " + nameResult);
+    System.out.println("Full name is: " + nameResult);
 
-	System.out.println("\nEnter another full name: ");
+	System.out.print("\nEnter another full name: ");
 	String fullName = scan.next();
 	
 	if (fullName.equalsIgnoreCase(nameResult)) {
         System.out.println("The names are the same.");
     } else {
-        System.out.println("The names are different.");
+        System.out.println("The two names are different.");
     }
 
-    System.out.println("Part 03;\n");
+    System.out.println("\nPart 03;\n");
     String replacedName = nameResult.replace('a', '@').replace('e', '3');
-    String.out.printf("Name after replacement: %s", replacedName);
+    System.out.printf("Name after replacement: %s\n", replacedName);
 
+    System.out.println("\nPart 04;");
+    String str = nameResult;
+    String[] parts = str.split(" ");
+    System.out.printf("\nFull Name Splitted;\n\t%s\n\t%s\n\t%s", parts[0], parts[1], parts[2]);
 
+    System.out.println("\nPart 05\n");
+    String trimmedString = stringWithSpaces.trim();
 	}
 }
