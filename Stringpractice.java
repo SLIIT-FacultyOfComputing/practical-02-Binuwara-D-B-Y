05704c147111374358c4baafa36d37b28d45319c
Binuwara-D-B-Y <IT23184558@my.sliit.lk>
2024-07-30 06:17:45 +0530

    Your commit message

--- Stringpractice.java
+++ Stringpractice.java
@@ -16,31 +16,47 @@ public class Stringpractice {
 	sb.append(firstName + " ");
     sb.append(middleName);
     String nameResult = sb.toString();
-	
     System.out.println("Full name is: " + nameResult);
 
-	System.out.print("\nEnter another full name: ");
-	String fullName = scan.next();
+    System.out.println("\nPart 02;");
+	System.out.print("Enter another full name: ");
+	scan.nextLine();
+    String fullName = scan.nextLine();
 	
 	if (fullName.equalsIgnoreCase(nameResult)) {
-        System.out.println("The names are the same.");
+        System.out.println("The two names are the same.");
     } else {
         System.out.println("The two names are different.");
     }
 
-    System.out.println("\nPart 03;\n");
+    System.out.println("\nPart 03;");
     String replacedName = nameResult.replace('a', '@').replace('e', '3');
     System.out.printf("Name after replacement: %s\n", replacedName);
 
     System.out.println("\nPart 04;");
     String str = nameResult;
     String[] parts = str.split(" ");
-    System.out.printf("\nFull Name Splitted;\n\t%s\n\t%s\n\t%s", parts[0], parts[1], parts[2]);
+    System.out.printf("Full Name Splitted;\n\t%s\n\t%s\n\t%s", parts[0], parts[1], parts[2]);
 
-    System.out.println("\nPart 05\n");
-    System.out.print("Enter a string with leading and trailing spaces: ");
-    Scanner stringWithSpaces = scan.next();
-    String trimmedString = stringWithSpaces.trim();
+    System.out.println("\n\nPart 05;");
+    System.out.println("Enter a string with leading and trailing spaces: ");
+    String strWithSpace = scan.nextLine();
+    String trimmedString = strWithSpace.trim();
     System.out.println(trimmedString);
+    scan.close();
+    
+    System.out.println("\nPart 06");
+    int vowelCount = 0; // Reset vowel count
+    String vowels = "aeiouAEIOU";
+    StringBuilder sbr = new StringBuilder(nameResult);
+
+    for (int i = 0; i < sb.length(); i++) {
+        char c = sbr.charAt(i);
+    if (vowels.indexOf(c) != -1) {
+        vowelCount++;
+        }
+    }
+    System.out.println("Concatenated Full name: " + nameResult);
+    System.out.println("Number of vowels in the concatenated full name: " + vowelCount);
 	}
 }
